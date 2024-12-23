59a2389362ca9d3aca1e00dd9b62bfb30d5c6f2d
trivindata <trivindata@gmail.com>
2024-12-23 19:47:07 +0530

    Assignment-1 DataTypes

--- src/com/creatio/crm/language/basics/DataTy.java
+++ src/com/creatio/crm/language/basics/DataTy.java
@@ -9,6 +9,73 @@ public class DataTy {
 		System.out.println("The Temperature of a city in degrees Celsius:"+degree);
 		
 		
+	    boolean customerOrder = true;
+	    System.out.println("Customer Place a Order:"+customerOrder );
+	    
+	    long PhoneNum = 9999974536l;
+	    System.out.println("The PhoneNumber is:"+ PhoneNum);
+		
+		
+		float Money = 1000.50f;
+		System.out.println("The Amount of money in a customer's bank account:" +Money);
+		
+		String Email = "automation@gmail.com";
+		System.out.println("The Person's email address:" +Email);
+		
+		
+		double Latitude =  37.7749;
+		double Longitude = -122.4194;
+		System.out.println("Coordinates of a Latitude:"+Latitude);
+		System.out.print("Coordinates of a Longitude:"+Longitude);
+		
+		boolean maritalStatus = true;
+	    System.out.println("Person's marital status:"+maritalStatus );
+	    
+	    String Occupation = "Software Engineer";
+		System.out.println("The Person's occupation:" +Occupation);
+		
+		String colour = "Blue";
+		System.out.println("The Person's favourite colour:" +colour);
+		
+		int currentYear =2023;
+		System.out.println("The Current Year:" +currentYear);
+		
+		double MediaPlatform = 100000;
+		System.out.println("Number of followers on a social media platform:" +MediaPlatform);
+		
+		double MovieRating = 7.5;
+		System.out.println("Rating of a movie: " +MovieRating);
+		
+		char BloodType = 'A';
+		System.out.println("Person's blood type:" +BloodType);
+		
+		String Book = "To Kill a Mockingbird";
+		System.out.println("Title of a book: " + Book);
+	
+		int company =500;
+		System.out.println("Number of employees in a company:" +company);
+		
+		String country = "United States";
+		System.out.println("Name of a country: " + country);
+		
+		String CountryName = "UnitedStates";
+		System.out.println("Name of a country:" + CountryName);
+		
+		String ColorType = "Brown";
+		System.out.println("Person's eye color: " +ColorType);
+		
+		String birthplace = "New York City";
+		System.out.println("Person's birthplace: " + birthplace);
+		
+		float cities = 200.5f;
+		System.out.println("Distance between two cities:" +cities);
+		
+		
+	
+
+		
+		
+		
 		
 		
 		
