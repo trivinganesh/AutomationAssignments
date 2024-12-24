03350eec3938b0ca4315a22601fad96b3713d6b2
trivindata <trivindata@gmail.com>
2024-12-24 19:43:40 +0530

    DataTypesAssgn1

--- src/com/creatio/crm/language/basics/DataTy.java
+++ src/com/creatio/crm/language/basics/DataTy.java
@@ -3,98 +3,88 @@ package com.creatio.crm.language.basics;
 public class DataTy {
 
 	public static void main(String[] args) {
-		
-		
-		//Temperature of a city in degrees Celsius
+
+		// Temperature of a city in degrees Celsius
 		double degree = 25.5;
-		System.out.println("The Temperature of a city in degrees Celsius:"+degree);
-		
-		//Whether a customer has placed an order
-	    boolean customerOrder = true;
-	    System.out.println("Customer Place a Order:"+customerOrder );
-	    
-	    //Person's phone number
-	    long PhoneNum = 9999974536l;
-	    System.out.println("The PhoneNumber is:"+ PhoneNum);
-		
-		//Amount of money in a customer's bank account
+		System.out.println("The Temperature of a city in degrees Celsius:" + degree);
+
+		// Whether a customer has placed an order
+		boolean customerOrder = true;
+		System.out.println("Customer Place a Order:" + customerOrder);
+
+		// Person's phone number
+		long PhoneNum = 9999974536l;
+		System.out.println("The PhoneNumber is:" + PhoneNum);
+
+		// Amount of money in a customer's bank account
 		float Money = 1000.50f;
-		System.out.println("The Amount of money in a customer's bank account:" +Money);
-		
-		//Person's email address
+		System.out.println("The Amount of money in a customer's bank account:" + Money);
+
+		// Person's email address
 		String Email = "john.doe@example.com";
-		System.out.println("The Person's email address:" +Email);
-		
-		//Coordinates of a location
-		double Latitude =  37.7749;
+		System.out.println("The Person's email address:" + Email);
+
+		// Coordinates of a location
+		double Latitude = 37.7749;
 		double Longitude = -122.4194;
-		System.out.println("Coordinates of a Latitude:"+Latitude);
-		System.out.print("Coordinates of a Longitude:"+Longitude);
-		
-		//Person's marital status
+		System.out.println("Coordinates of a Latitude:" + Latitude);
+		System.out.print("Coordinates of a Longitude:" + Longitude);
+
+		// Person's marital status
 		boolean maritalStatus = true;
-	    System.out.println("Person's marital status:"+maritalStatus );
-	    
-	    //Person's occupation
-	    String Occupation = "Software Engineer";
-		System.out.println("The Person's occupation:" +Occupation);
-		
-		//Person's favourite colour
+		System.out.println("Person's marital status:" + maritalStatus);
+
+		// Person's occupation
+		String Occupation = "Software Engineer";
+		System.out.println("The Person's occupation:" + Occupation);
+
+		// Person's favourite colour
 		String colour = "Blue";
-		System.out.println("The Person's favourite colour:" +colour);
-		
-		//Current year
-		int currentYear =2023;
-		System.out.println("The Current Year:" +currentYear);
-		
-		
-		//Number of followers on a social media platform
+		System.out.println("The Person's favourite colour:" + colour);
+
+		// Current year
+		int currentYear = 2023;
+		System.out.println("The Current Year:" + currentYear);
+
+		// Number of followers on a social media platform
 		String MediaPlatform = "1,000,000";
-		System.out.println("Number of followers on a social media platform:" +MediaPlatform);
-		
-		//Rating of a movie
+		System.out.println("Number of followers on a social media platform:" + MediaPlatform);
+
+		// Rating of a movie
 		double MovieRating = 7.5;
-		System.out.println("Rating of a movie: " +MovieRating);
-		
-		//Person's blood type
+		System.out.println("Rating of a movie: " + MovieRating);
+
+		// Person's blood type
 		char BloodType = 'A';
-		System.out.println("Person's blood type:" +BloodType);
-		
-		//Title of a book
+		System.out.println("Person's blood type:" + BloodType);
+
+		// Title of a book
 		String Book = "To Kill a Mockingbird";
 		System.out.println("Title of a book: " + Book);
-	
-		//Number of employees in a company
-		int company =500;
-		System.out.println("Number of employees in a company:" +company);
-		
-		//Name of a country
+
+		// Number of employees in a company
+		int company = 500;
+		System.out.println("Number of employees in a company:" + company);
+
+		// Name of a country
 		String country = "United States";
 		System.out.println("Name of a country: " + country);
-		 
-		//Person's eye color
+
+		// Person's eye color
 		String ColorType = "Brown";
-		System.out.println("Person's eye color: " +ColorType);
-		
-		//Person's birthplace
+		System.out.println("Person's eye color: " + ColorType);
+
+		// Person's birthplace
 		String birthplace = "New York City";
 		System.out.println("Person's birthplace: " + birthplace);
-		
-		//Distance between two cities
-		float cities = 200.5f;
-		System.out.println("Distance between two cities:" +cities);
-		
-		//Time of an event:
-		String Time = "2.30 PM";
-		System.out.println("Person's birthplace: " + Time);
-	
 
-		
-		
-		
-		
-		
-		
+		// Distance between two cities
+		float cities = 200.5f;
+		System.out.println("Distance between two cities:" + cities);
+
+		// Time of an event:
+		String Time = "2.30 PM";
+		System.out.println("Time of an event: " + Time);
 
 	}
 
